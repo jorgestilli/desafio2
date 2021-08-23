8aebc0e7501b9f670916c91041bfbd0b26f480a3
jorgestilli <89410144+jorgestilli@users.noreply.github.com>
2021-08-23 13:57:30 -0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-var numero= parseInt(prompt("Ingrese un numero del 1 al 10"))
-var numero2= parseInt(prompt("Ingrese otro numero del 1 al 10"))
-
-if(numero>numero2){
-    alert(numero+" ES MAYOR QUE "+ numero2)
-}
-else {
-    alert(numero2+ " ES MAYOR QUE "+ numero)
-}
